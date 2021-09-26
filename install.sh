7b3f09c1c0bf4d7d646a2e8585271c12a5c8d170
Abby Snellman <abby.snellman@hotmail.com>
2021-09-26 17:26:33 -0400

    Changing line endings in install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get upgrade
 
 sudo apt -y install python3-pip python3-cffi
 sudo pip3 install discord.py[voice]
-sudo pip3 python-dotenv
+sudo pip3 install python-dotenv
 
 
 echo "Creating Movie Catalog Files" 
@@ -21,7 +21,7 @@ sudo cp moviebot.service /etc/systemd/system
 sudo chmod 755 /etc/systemd/system/moviebot.service
 sudo chown root:root /etc/systemd/system/moviebot.service
 sudo systemctl enable moviebot.service
-sudo systemctl restart vwebserver
+sudo systemctl restart moviebot
 cd -
 
 
